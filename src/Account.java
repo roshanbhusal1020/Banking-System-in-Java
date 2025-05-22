cef567c58127bab216f609e3a65e8011ed2599b7
Roshan Bhusal <rosan_wanted@yahoo.com>
2025-05-22 16:23:58 +0100

    can create signup, deposit and widthdraw money

---
+++ src/Account.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class Account {
+
+    private String username;
+    private String id;
+    private int passcode;
+    private double balance;
+
+
+    public Account (String username, int passcode) {
+        this.username = username;
+//        this.id = id;
+        this.passcode = passcode;
+//        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        this.balance += amount;
+
+    }
+    public void withdraw(double amount) {
+        this.balance -= amount;
+    }
+
+    public void display() {
+        if (Double.isNaN(balance)){
+            System.out.println("No balance");
+            System.out.println("Username = " +username + "\n Password = " + passcode );
+        }
+        else {
+            System.out.println("Total balance: " + this.balance);
+            System.out.println("Username = " +username + "\n Password = " + passcode );
+
+        }
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,70 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.ArrayList;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    static ArrayList<Account> accounts = new ArrayList<Account>();
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Welcome to Banking System\n" + "1. Sign up\n" + "2. Log in\n" + "3. Exit\n");
+        String choice = sc.nextLine();
+
+        if (choice.equals("1")) {
+            signup(sc);
+        } else if (choice.equals("2")) {
+            login(sc);
+        } else if (choice.equals("3")) {
+            System.out.println("Thank you for using Banking System");
+            return;
         }
+
+    }
+
+
+    public static void signup(Scanner sc) {
+        System.out.println("Welcome to Banking System");
+        System.out.println("Please enter your username: ");
+        String username = sc.nextLine();
+        System.out.println("Please enter your passcode: ");
+        Integer passcode = sc.nextInt();
+        sc.nextLine();
+
+        Account ac = new Account(username, passcode );
+        accounts.add(ac);
+        ac.display();
+        operations(sc, ac);
+
+    }
+
+    public static void  operations(Scanner sc, Account ac) {
+        while (true) {
+            String input = sc.nextLine();
+
+            switch (input) {
+                case "deposit": Double depositValue = sc.nextDouble();
+                                sc.nextLine();
+                                ac.deposit(depositValue);
+                                System.out.println("Deposited: " + depositValue );
+                                ac.display();
+                                break;
+                case "withdraw": Double withdrawValue = sc.nextDouble();
+                                sc.nextLine();
+                                ac.withdraw(withdrawValue);
+                                System.out.println("Withdrawn: " + withdrawValue );
+                                ac.display();
+                                break;
+
+            }
+
+        }
+    }
+    public static void login (Scanner sc) {
+        System.out.println("Welcome to Banking System\n Plese enter your username: ");
+        String username = sc.nextLine();
+        System.out.println("Please enter your passcode: ");
+        String passcode = sc.nextLine();
     }
 }
